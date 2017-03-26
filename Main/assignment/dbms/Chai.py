932c1cccabe5a519e5089e229e6885310f6c6317
tomMoulard <tom.moulard@epita.fr>
2017-03-26 11:02:56 +0800

    nearly there

--- Main/assignment/dbms/Chai.py
+++ Main/assignment/dbms/Chai.py
@@ -9,10 +9,47 @@ Created on Tue Mar 14 23:07:26 2017
 import urllib.request
 opener = urllib.request.FancyURLopener({})
 
-#to get users imput
-import sys
+#Random
+import random
 
 URL = "http://www.mysqltutorial.org/basic-mysql-tutorial.aspx"
+
+def getLineCode(responce, pos):
+    """
+    Get the whole code from a box.
+    The pos must be set just after the "crayon-code" (aka the begining of the box)
+    return (line, newPos) as newPos an updated position is the responce
+    """
+    line = []
+    lines = []
+    ll = len(responce)
+    while pos < ll: 
+        if "crayon-line" == responce[pos:pos+11]:
+            pos += 11
+            if "<span" == response[pos:pos+5]:
+                while pos < ll and responce[pos] != ">":
+                    pos += 1
+                
+        pos += 1
+    return line, pos
+
+
+def getCodeFromUrl(url):
+    """
+    Get the whole code from the page
+    return an array of all the code on dat page
+    """
+    responce = str(opener.open(URL).read())
+    code = []
+    pos = 0
+    ll = len(response)
+    while pos < ll:
+        if "crayon-code" == responce[pos:pos+11]:
+            pos += 11
+            line, pos = getLineCode(responce, pos)
+            code.append(line)
+        pos += 1
+    return code
 
 def main():
         responce = str(opener.open(URL).read())
@@ -23,10 +60,33 @@ def main():
         pos  = 0
         ll   = len(responce)
         while pos < ll:
-            #Iterate thru the whole responce
+            #Iterate thru the whole responce to fill urls
+            if "<article" == responce[pos:pos+8]:
+                #inside the right container
+                while pos < ll:
+                    if "</article" == responce[pos:pos+9]:
+                        #out of the right container
+                        break
+                    else:
+                        if "http://www.mysqltutorial.org/" == responce[pos:pos+29]:
+                            #This is a link in the page
+                            zelda = ""
+                            while pos < ll and responce[pos] != "\"":
+                                zelda += responce[pos]
+                                pos += 1
+                            urls.append(zelda)
+                    pos +=1
             pos += 1
         print("links get")
+        #remove crappy urls
+        urls = urls[3:len(urls) - 9]
         #Iterate thru all urls to get all code
+        code = []
+        for x in range(len(urls));
+            print("getting code for", urls[x], "(", x, ")")
+            tmp = getCodeFromUrl(urls[x])
+            for y in tmp:
+                code.append(y)
         #connect to server
         #lauch code with random interval (5 - 20s between)
 
