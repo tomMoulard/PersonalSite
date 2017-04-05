d634dcbfbe3f97a5ca493f1ba95b8b838e416f71
tomMoulard <tom.moulard@epita.fr>
2017-04-05 21:47:35 +0800

    nearly there

--- Main/dates/sketch.js
+++ Main/dates/sketch.js
@@ -1,14 +1,70 @@
 var h,w;
 var file;
+var urlOfFile;
 function setup() {
     h = windowHeight;
     w = windowWidth;
-    createCanvas(w,h);
-    background(0);
-    file = FileReader(../../date.txt);
+    //to get informations
+    urlOfFile = "http://tom.moulard.org/dates/date.txt";
     console.log(file);
-}
 
-function draw() {
-    
+    //to set the graph
+    //$.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=usdeur.json&callback=?', function (data) {
+    $.getJSON(urlOfFile, function (data) {
+        Highcharts.chart('container', {
+            chart: {
+                zoomType: 'x'
+            },
+            title: {
+                text: 'Date of self update'
+            },
+            subtitle: {
+                text: document.ontouchstart === undefined ?
+                        'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in'
+            },
+            xAxis: {
+                type: 'datetime'
+            },
+            yAxis: {
+                title: {
+                    text: 'Hour of the day'
+                }
+            },
+            legend: {
+                enabled: false
+            },
+            plotOptions: {
+                area: {
+                    fillColor: {
+                        linearGradient: {
+                            x1: 0,
+                            y1: 0,
+                            x2: 0,
+                            y2: 1
+                        },
+                        stops: [
+                            [0, Highcharts.getOptions().colors[0]],
+                            [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                        ]
+                    },
+                    marker: {
+                        radius: 2
+                    },
+                    lineWidth: 1,
+                    states: {
+                        hover: {
+                            lineWidth: 1
+                        }
+                    },
+                    threshold: null
+                }
+            },
+
+            series: [{
+                type: 'area',
+                name: 'hour',
+                data: data
+            }]
+        });
+    });
 }
