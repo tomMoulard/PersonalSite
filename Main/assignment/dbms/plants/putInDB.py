b929af36e8d70341bc5c50cf9561652569e5a51a
tomMoulard <tom.moulard@epita.fr>
2017-04-12 16:22:59 +0800

    Adding a good way to push data in the db This part is done (Finaly)

--- Main/assignment/dbms/plants/putInDB.py
+++ Main/assignment/dbms/plants/putInDB.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Apr 6 13:00:00 2017
-
+@python3
 @author: tm
 The goal is to send data to a db
 data should be an array of tupple : ("<Name Of Column>","<Content To send>")
@@ -35,15 +35,16 @@ import re
 
 #credentials
 #mysql -u 16920041 -h 172.31.34.145 -p
-SN       = "16920041"
-USER     = SN
-SERVER   = "172.31.34.145"
-PASSWORD = SN
-DB       = "db" + SN
-CURSOR   = None;
-CONFIG   = "./CONFIG"
-PDFS     = "/tmp/plant_pdfs" 
-TABLE    = "usdaplant"
+SN          = "16920041"
+USER        = SN
+SERVER      = "172.31.34.145"
+PASSWORD    = SN
+DB          = "db" + SN
+CURSOR      = None;
+CONFIG      = "./CONFIG"
+PDFS        = "/tmp/plant_pdfs" 
+TABLE       = "usdaplant"
+PRIMARYKEYS = []
 
 def prettyPrintForList(l):
     """
@@ -80,13 +81,7 @@ def gettingCredsForDB():
             + str(t[4]) + "m"\
             + str(t[5]) + "s_"\
             + "plants"
-    TABLE = str(t[0]) + "_"\
-        + str(t[1]) + "_"\
-        + str(t[2]) + "_"\
-        + str(t[3]) + "h"\
-        + str(t[4]) + "m"\
-        + str(t[5]) + "s_"\
-        + "plants"
+    TABLE = "usdaplant"
     return USER, SERVER, PASSWORD, DB, PDFS, TABLE
 
 #To get the Credentials
@@ -106,13 +101,18 @@ def exe(code, optionnalOutput=""):
     except:
         print(sys.exc_info())
 
-def printResponce():
+def printTable(tableMeta):
     """
-    This is supposed to print the responce of the db
+    This is supposed to print the response of the db
     """
+    exe("SELECT * FROM " + TABLE)
+    for col in tableMeta[:-1]:
+        print(col[0], end=" -> ")
+    print(tableMeta[-1][0])
     try:
-        for line in CURSOR.fetchall():
-            for words in line[-1]:
+        response = CURSOR.fetchall()
+        for line in response:
+            for words in line[:-1]:
                 print(words, end=" -> ")
             print(line[-1])
     except:
@@ -120,13 +120,23 @@ def printResponce():
         print(sys.exc_info())
 
 
-def changeColSize(colPos, newSize):
+def changeColSize(colName, newSize, tableMeta):
     """
-    This function change the colName size
+    This function change the colName size to newSize
+    and update tableMeta
     """
-    val = "ALTER TABLE " + TABLE + " MODIFY " + colPos + " " +\
-        TYPECOLS[colPos] + "(" + newSize + ");"
-    LENCOLS[colPos] = max(newSize, LENCOLS[colPos])
+    #get the pos in the tableMeta
+    pos = 0
+    for cols in range(len(tableMeta)):
+        if colName == tableMeta[cols][0]:
+            pos = cols
+            break
+    #getting the bigger value
+    tableMeta[pos] =\
+        (tableMeta[pos][0], tableMeta[pos][1], max(newSize, int(tableMeta[pos][2])))
+    #setting the query
+    val = "ALTER TABLE " + TABLE + " MODIFY " + tableMeta[pos][0] + " " +\
+        tableMeta[pos][1] + "(" + str(tableMeta[pos][2]) + ");"
     exe(val)
 
 def sendDataToDB(data):
@@ -171,16 +181,93 @@ def sendDataToDB(data):
                     changeColSize(x, len(data[x]))
                 var += ", " + COLS[x] + " = " + data[x]
             exe(var + ";")
-        
-def addCol(colName, typeCols="VARCHAR", size="64"):
+
+def isAlreadyThere(primaryKey):
+    """
+    This check the db to know if the key is already there
+    """
+    return primaryKey in PRIMARYKEYS
+
+def checkSizeAndModify(colName, size, tableMeta):
+    """
+    This is supposed to check if the column is bit enought
+    if not, just take nore space
+    #By the way, to col should be in the table
+    No return
+    """
+    #find colname in tableMeta and get this size
+    pos = 0
+    for col in range(len(tableMeta)):
+        if colName == tableMeta[col][0]:
+            pos = col
+            break
+    #if it is not big enought
+    if int(tableMeta[pos][2]) < size:
+        #update the table
+        tableMeta[pos] = (tableMeta[pos][0], tableMeta[pos][1], str(size + 1))
+        #and update the tableMeta
+        changeColSize(colName, int(tableMeta[pos][2]), tableMeta)
+
+
+def sendDataToDB2(data, tableMeta):
+    """
+    This is used so select which data and who to send the to the db
+    the table should be ready to accept data
+    data format: [("<column name>", "<content>")]
+    MUST PROVIDE tableMeta to get table informations
+    [("<Col Name>", "<Col Type>", "<Col Size>")]
+    if the symbol is already there just add the data from the pdf
+    otherwise add all and and add primary key to PRIMARYKEYS
+    """
+    # create new columns
+    for d in data:
+        found = False
+        for col in tableMeta:
+            if d[0] == col[0]: # the col exist
+                found = True
+                break
+        if not found:
+            addCol(d[0], tableMeta, size=str(len(d[1]) + 1))
+    #insert data
+    #if already there just add 
+    INS =  "INSERT INTO " + TABLE + "("
+    if isAlreadyThere(data[0][1]):
+        #USE DICT YOU DICK .....
+        #Find "fs_Alternative_Name" and check his value
+        if data[3][1] == "": #insert just 5/6
+            checkSizeAndModify(data[5][0], len(data[d][1]), tableMeta)
+            checkSizeAndModify(data[6][0], len(data[d][1]), tableMeta)
+            INS = data[5][0]  + " " + data[6][0] +\
+                ") WHERE " + data[0][0] + " = " + data[0][1] + \
+                "VALUES ( "+ data[5][0]  + " " + data[6][0] + ");"
+        else:
+            checkSizeAndModify(data[3][0], len(data[d][1]), tableMeta)
+            checkSizeAndModify(data[4][0], len(data[d][1]), tableMeta)
+            INS = data[3][0]  + " " + data[4][0] +\
+                ") WHERE " + data[0][0] + " = " + data[0][1] + \
+                "VALUES ( "+ data[3][0]  + " " + data[4][0] + ");"
+        exe(INS)
+    else: #insert all the data
+        VAL = ""
+        for d in range(len(data[:-1])):
+            INS += data[d][0] + ", "
+            VAL += "\"" + data[d][1] + "\", "
+            checkSizeAndModify(data[d][0], len(data[d][1]), tableMeta)
+        INS += data[-1][0] + ") VALUES ("
+        VAL += "\"" + data[-1][1] + "\");"
+        exe(INS + VAL)
+        #instert key in the PRIMARYKEYS array
+        PRIMARYKEYS.append(data[0][1])
+        #sendDataToDB2(generateFakeData(), COLSMETA)
+
+
+def addCol(colName, tableMeta, typeCols="VARCHAR", size="64"):
     """
     This symply add a new colone to the TABLE
     Called colName
     and update COLS
     """
-    COLS.append(colName)
-    LENCOLS.append(size)
-    TYPECOLS.append(typeCols)
+    tableMeta.append((colName, typeCols, size))
     exe("ALTER TABLE "+TABLE+" ADD COLUMN "+colName+" "+typeCols+"("+size+");")
 
 def goToDB():
@@ -213,12 +300,15 @@ def goToTable(tableMeta):
     CREATETABLE += tableMeta[0][0] + " " + tableMeta[0][1] +\
             "(" + tableMeta[0][2] + ") PRIMARY KEY NOT NULL"
     #adding all knowned columns
-    for x in range(1, len(data)):
-        CREATETABLE += ", " + tableMeta[x] + " = " + tableMeta[x]
-    CREATETABLE += ") ENGINE=InnoDB"
+    for x in range(1, len(tableMeta)):
+        CREATETABLE += ", " + tableMeta[x][0] + " " + tableMeta[x][1] +\
+            "(" + str(tableMeta[x][2]) + ")"
+    CREATETABLE += ") ENGINE=InnoDB; "
     exe(CREATETABLE)    
 
 def main():
+    goToDB()
+    goToTable([])
     print("Table", TABLE, "Created")
     print("Opening pdfs parse them and store data in the db")
     files = glob.glob(PDFS + "*.pdf")
@@ -227,6 +317,6 @@ def main():
         d = getDataFromPDF(files[file])
         sendDataToDB(d)
     exe("SHOW TABLES;")
-    printResponce()
+    printTable()
     print("closing DB")
     db.close()
