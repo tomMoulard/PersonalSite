1aa86cf2742e60cc714b19f7df12460240f2db2b
tomMoulard <tom.moulard@epita.fr>
2017-05-08 14:27:05 +0800

    proper indentation + proper way to manage click inside the window you can now click the checkbox or the slider without creating a new walker added a slider to adjust the strockWeight of points

--- Main/fun/2/sketch.js
+++ Main/fun/2/sketch.js
@@ -2,44 +2,51 @@ var h, w;
 var walkers = [];
 var points = [];
 var checkSym4;
+var strokeW;
 function setup() {
-	h = windowHeight - 20;
-	w = windowWidth;
-	createCanvas(w, h);
-	stroke(0,255,0);
-	strokeWeight(4);
-	checkSym4 = createCheckbox();
+    h = windowHeight - 44;
+    w = windowWidth;
+    createCanvas(w, h);
+    stroke(0,255,0);
+    strokeWeight(3);
+    checkSym4 = createCheckbox();
+    strokeW = createSlider(0, 20, 3);
 }
 
 function draw() {
-	background(0);
-	for(var i = 0; i < walkers.length; i++){
-		walkers[i].move();
-	}
-	for(var j = 0; j < points.length; j++){
-		print(points[j].x);
-		point(points[j].x, points[j].y);
-	}
+    strokeWeight(strokeW.value());
+    background(0);
+    for(var i = 0; i < walkers.length; i++){
+        walkers[i].move();
+    }
+    for(var j = 0; j < points.length; j++){
+        //print(points[j].x);
+        point(points[j].x, points[j].y);
+    }
 }
 
 function mousePressed(){
-		add(mouseX, mouseY);
+    if (mouseX < w && mouseY < h){
+        add(mouseX, mouseY);
+    }
 }
 function mouseDragged(){
-	add(mouseX, mouseY);	
+    if (mouseX < w && mouseY < h){
+        add(mouseX, mouseY);    
+    }
 }
 
 function add(x, y){
-	var newwalker = new Walker(x, y, checkSym4.checked());
-	append(walkers, newwalker);
+    var newwalker = new Walker(x, y, checkSym4.checked());
+    append(walkers, newwalker);
 }
 
 function addL(x, y){
-	var here = new P(x,y);
-	append(points, here);
+    var here = new P(x,y);
+    append(points, here);
 }
 
 function P(x,y) {
-	this.x = x;
-	this.y = y;
+    this.x = x;
+    this.y = y;
 }
