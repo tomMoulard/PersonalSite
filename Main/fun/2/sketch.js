a8dbfb8f0edc02832d941eb30fc5a52fd091d96c
tomMoulard <tom.moulard@epita.fr>
2017-05-08 14:28:12 +0800

    prettyfy

--- Main/fun/2/sketch.js
+++ Main/fun/2/sketch.js
@@ -3,11 +3,12 @@ var walkers = [];
 var points = [];
 var checkSym4;
 var strokeW;
+
 function setup() {
     h = windowHeight - 44;
     w = windowWidth;
     createCanvas(w, h);
-    stroke(0,255,0);
+    stroke(0, 255, 0);
     strokeWeight(3);
     checkSym4 = createCheckbox();
     strokeW = createSlider(0, 20, 3);
@@ -16,37 +17,38 @@ function setup() {
 function draw() {
     strokeWeight(strokeW.value());
     background(0);
-    for(var i = 0; i < walkers.length; i++){
+    for (var i = 0; i < walkers.length; i++) {
         walkers[i].move();
     }
-    for(var j = 0; j < points.length; j++){
+    for (var j = 0; j < points.length; j++) {
         //print(points[j].x);
         point(points[j].x, points[j].y);
     }
 }
 
-function mousePressed(){
-    if (mouseX < w && mouseY < h){
+function mousePressed() {
+    if (mouseX < w && mouseY < h) {
         add(mouseX, mouseY);
     }
 }
-function mouseDragged(){
-    if (mouseX < w && mouseY < h){
-        add(mouseX, mouseY);    
+
+function mouseDragged() {
+    if (mouseX < w && mouseY < h) {
+        add(mouseX, mouseY);
     }
 }
 
-function add(x, y){
+function add(x, y) {
     var newwalker = new Walker(x, y, checkSym4.checked());
     append(walkers, newwalker);
 }
 
-function addL(x, y){
-    var here = new P(x,y);
+function addL(x, y) {
+    var here = new P(x, y);
     append(points, here);
 }
 
-function P(x,y) {
+function P(x, y) {
     this.x = x;
     this.y = y;
 }
