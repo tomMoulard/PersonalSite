05739ac16f11e1dff63d2ba7f824f709fa59a51d
tomMoulard <tom.moulard@epita.fr>
2017-10-13 01:27:29 +0200

    changing the style of the index page adding the chatbot (not the prettiest) removing  service worker addning some scripts for the chatbox

---
+++ Main/assets/js/chatbot.js
@@ -0,0 +1,37 @@
+function setup() {
+    noCanvas();
+
+    let bot = new RiveScript();
+    let conv = "";
+    //bot.loadFile("http://tom.moulard.org/assets/brains/EN-tom.rive", brainReady, brainError);
+    bot.loadFile("assets/brains/EN-tom.rive", brainReady, brainError);
+
+    // let button = select("#submit");
+    let user_input = select('#user_input');
+    let response = select("#dialogue");
+
+    user_input.changed(function (e) {
+            ask(user_input.value().toSring());
+            user_input.value("");
+    });
+
+    // button.mousePressed(chat);
+
+    function brainReady(){
+        console.log("Chat bot ready!");
+        bot.sortReplies();
+    }
+
+    function brainError(){
+        console.log("Chat bot ERROR!");
+    }
+    function ask(things){
+        conv += "<font color='blue'><p><b>You</b>: " + things + "</p></font>";
+        response.html(conv);
+        anwser(bot.reply("local-user", things));
+    }
+    function anwser(things){
+        conv += "<font color='red'><p><b>Bot</b>: " + things + "</p></font>";
+        response.html(conv);
+    }
+}
