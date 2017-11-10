e22b707ecd45875fc4ac88b71b18419630542f43
tomMoulard <tom.moulard@epita.fr>
2017-11-10 17:28:44 +0100

    Update setup.sh

--- Main/setup/setup.sh
+++ Main/setup/setup.sh
@@ -1,22 +1,3 @@
 #!/bin/bash
 
-#To use:
-# curl http://tom.moulard.org/setup/setup.sh | bash
-
-if [ "$HOME/.bashrc" ]
-then
-    #put git:bashrc > .bashrc
-    curl https://raw.githubusercontent.com/tomMoulard/configLoader/master/bashrc > $HOME/.bashrc
-    echo "Bashrc imported!"
-    #put git:alias > .aliases
-    curl https://raw.githubusercontent.com/tomMoulard/configLoader/master/aliases >> $HOME/.bashrc
-    echo "Aliases imported!"
-    source $HOME/.bashrc
-
-    echo Done!
-elif [ "$HOME/.zshrc" ]
-then
-    echo zshrc
-else
-    echo not recognized
-fi
+curl https://raw.githubusercontent.com/tomMoulard/configLoader/master/setup.sh | sh
